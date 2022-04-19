941177219ac9b927afb19f02effc60eeaf1a7021
shaha <shaha0111@mail.ru>
2022-04-19 09:11:59 +0600

    extra code deleted

--- provider_a.py
+++ provider_a.py
@@ -6,12 +6,7 @@ from fastapi.responses import FileResponse
 app = FastAPI()
 
 
-@app.get("/")
-def index ():
-    return "Hello world!"
-
-
 @app.post("/search/")
-async def read_file():
-    # time.sleep(30)
+def read_file():
+    time.sleep(30)
     return FileResponse("response_a.json")
--- provider_b.py
+++ provider_b.py
@@ -7,12 +7,6 @@ app = FastAPI()
 
 
 @app.post("/search/")
-async def read_file():
-    # time.sleep(60)
+def read_file():
+    time.sleep(60)
     return FileResponse("response_b.json")
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile("response_b.json")):
-    contents = await file.read()
-    return contents
